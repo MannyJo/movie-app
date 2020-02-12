4d860b0b79e8a8750d80a0cb86bc2b7e07827998
Manny Jo <jomansang@gmail.com>
2020-02-12 16:05:58 -0600

    Update setting.py

--- server/server/settings.py
+++ server/server/settings.py
@@ -131,8 +131,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-django_heroku.settings(locals())
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
